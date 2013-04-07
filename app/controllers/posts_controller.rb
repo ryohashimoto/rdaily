d526a7c37f9fd03d498fa875f1b47c56b2302e16
Ryo Hashimoto <ryohashimoto@gmail.com>
2013-04-07 16:25:44 +0900

    add posts#index layout and published scope

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
 
   def index
-    @posts = Post.all
+    @posts = Post.published.order(:created_at).reverse_order
   end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,9 @@
 class Post < ActiveRecord::Base
   belongs_to :user
   
-  attr_accessible :title, :body, :published_at, :published
+  attr_accessible :title, :body, :published_at
 
   validates :title, :body, :user_id, :presence => true
+
+  scope :published, where("published_at not ?", nil)
 end
