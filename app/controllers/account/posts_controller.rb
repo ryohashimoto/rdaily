2c986daf801121342c69a48a557404b1e42c2cf6
Ryo Hashimoto <ryohashimoto@gmail.com>
2013-04-07 14:05:32 +0900

    add current_user and account/posts controller

---
+++ app/controllers/account/posts_controller.rb
@@ -0,0 +1,19 @@
+class Account::PostsController < ApplicationController
+  def index
+    @posts = current_user.posts.all
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(params[:post])
+    @post.user_id = current_user.id
+    if @post.save
+      redirect_to :index
+    else
+      render :new
+    end
+  end
+end
