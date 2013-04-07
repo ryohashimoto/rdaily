a2df70d7369ea0189411c6ab294669ed9ad9f6ba
Ryo Hashimoto <ryohashimoto@gmail.com>
2013-04-07 19:16:59 +0900

    add methods to account/categories controller

--- app/controllers/account/categories_controller.rb
+++ app/controllers/account/categories_controller.rb
@@ -1,6 +1,43 @@
 class Account::CategoriesController < ApplicationController
   def index
-    
+    @categories = resources.order('name asc')
   end
 
+  def new
+    @category = resources.new
+  end
+
+  def create
+    @category = resources.new(params[:category])
+    if @category.save
+      flash[:notice] = 'Category is successfully created.'
+    else
+      flash[:alert] = 'Category is not created.'
+    end
+    redirect_to account_categories_path
+  end
+
+  def edit
+    @category = resources.find(params[:id])
+  end
+
+  def update
+    @category = resources.find(params[:id])
+    if @params[:category] && @cateogry.update_attributes(params[:category])
+      redirect_to account_categories_path
+    else
+      render :action => :edit
+    end
+  end
+  
+  def destroy
+    @category = resources.find(params[:id])
+    if @category.destroy
+      redirect_to account_categories_path      
+    end
+  end
+
+  def resources
+    current_user.categories
+  end
 end
--- app/models/category.rb
+++ app/models/category.rb
@@ -2,6 +2,7 @@ class Category < ActiveRecord::Base
   attr_accessible :name
 
   validates :name, :uniqueness => true, :presence => true
+  validates :user_id, :presence => true
   has_many :categorizations
   has_many :posts, :through => :categorizations
   belongs_to :user
