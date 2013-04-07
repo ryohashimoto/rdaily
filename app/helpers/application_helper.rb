e720540203641e0b5ae733df68914c6dfa12317c
Ryo Hashimoto <ryohashimoto@gmail.com>
2013-04-07 18:09:58 +0900

    add abbrev helper method and use collection partials

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def abbrev(text, max=100)
+    if text.length >= max
+      simple_format(text[0..max-1] + '...')
+    else
+      simple_format(text)
+    end
+  end
 end
