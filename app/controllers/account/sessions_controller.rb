c516c78a092ea7ce3bc3c7496a22449daf41bb97
Ryo Hashimoto <ryohashimoto@gmail.com>
2013-04-07 13:24:55 +0900

    add account/sessions controller

---
+++ app/controllers/account/sessions_controller.rb
@@ -0,0 +1,18 @@
+class Account::SessionsController < ApplicationController
+
+  def create
+    user = User.find_by_email(params[:email])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to account_path, :notice => "Logged in as #{user.name}."
+    else
+      flash[:alert] = "Invalid email/password pair."
+      render 'new'
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to account_path, notice => "Logged out."
+  end
+end
---
+++ app/helpers/account/sessions_helper.rb
@@ -0,0 +1,2 @@
+module Account::SessionsHelper
+end
