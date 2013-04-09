42edf8ab45cf25aec1d265a3e03f4aaa9037d848
Ryo Hashimoto <ryohashimoto@gmail.com>
2013-04-09 12:52:18 +0900

    add slug to categories

--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,17 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130407095523) do
+ActiveRecord::Schema.define(:version => 20130409035000) do
 
   create_table "categories", :force => true do |t|
     t.string   "name"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
     t.integer  "user_id"
+    t.string   "slug"
   end
+
+  add_index "categories", ["slug"], :name => "index_categories_on_slug"
 
   create_table "categorizations", :force => true do |t|
     t.integer  "post_id"
