f82bcc8ce20b3f5e68dd421a99aa721b24ced582
Ryo Hashimoto <ryohashimoto@gmail.com>
2013-04-09 13:00:14 +0900

    generate slug for categories

--- app/models/category.rb
+++ app/models/category.rb
@@ -3,7 +3,18 @@ class Category < ActiveRecord::Base
 
   validates :name, :uniqueness => true, :presence => true
   validates :user_id, :presence => true
+  validates :slug, :uniqueness => true, :presence => true
+  before_validation :generate_slug
+  
   has_many :categorizations
   has_many :posts, :through => :categorizations
   belongs_to :user
+
+  def to_param
+    plug
+  end
+
+  def generate_slug
+    self.slug ||= name.parameterize
+  end
 end
--- db/migrate/20130409035000_add_slug_to_categories.rb
+++ db/migrate/20130409035000_add_slug_to_categories.rb
@@ -1,6 +1,15 @@
 class AddSlugToCategories < ActiveRecord::Migration
-  def change
-    add_column :categories, :slug, :string
+  def self.up
+    add_column :categories, :slug, :string, :after => :name
     add_index :categories, :slug
+
+    Category.all.each do |category|
+      category.save!
+    end      
+  end
+
+  def self.down
+    remove_index :categories, :slug
+    remove_column :categories, :slug
   end
 end
