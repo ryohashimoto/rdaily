b2e4198da2b4fe374ebf69708b5f1f591e5e9589
Ryo Hashimoto <ryohashimoto@gmail.com>
2013-05-15 10:01:06 +0900

    add user model specs

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,8 +1,26 @@
 require 'spec_helper'
 
 describe User do
-  it "is valid with a name and email"
-  it "is invalid without a name"
-  it "is invalid without an email"
-  it "is invalid with a duplicate email address"
+  it "is valid with a name, email and password" do
+    user = User.new(name: 'ryo', email: 'ryo@example.com', password: 'hogehoge')
+    expect(user).to be_valid
+  end
+  it "is invalid without a name" do
+    user = User.new(email: 'ryo@example.com', password: 'hogehoge')
+    expect(user).to be_invalid
+  end
+  it "is invalid without an email" do
+    user = User.new(name: 'ryo', password: 'hogehoge')
+    expect(user).to be_invalid    
+  end
+  it "is invalid without a password" do
+    user = User.new(name: 'ryo', email: 'ryo@example.com')
+    expect(user).to be_invalid
+  end
+  it "is invalid with a duplicate email address" do
+    user1 = User.new(name: 'ryo', email: 'ryo@example.com', password: 'hogehoge')
+    expect(user1.save!).to be_true
+    user2 = User.new(name: 'hashimoto', email: 'ryo@example.com', password: 'hogehoge')
+    expect(user2).to be_invalid
+  end
 end
