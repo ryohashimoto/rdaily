2a3a60a7fea452fcc032ae6e9b9a48d3c7cb0946
Ryo Hashimoto <ryohashimoto@gmail.com>
2013-05-15 11:20:02 +0900

    add post model spec

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,8 +1,30 @@
 require 'spec_helper'
 
 describe Post do
-  it "is valid with a title, body, user_id"
-  it "is invalid without a title"
-  it "is invalid without a body"
-  it "is invalid without a user_id"
+  before :each do
+    @user = User.create(name: 'ryo', email: 'ryo@example.com', password: 'hogehoge')
+  end
+  
+  it "is valid with a title, body, user_id" do
+    post = @user.posts.build
+    post.title = 'hoge'
+    post.body = 'foobar'
+    expect(post).to be_valid
+  end
+
+  it "is invalid without a title" do
+    post = @user.posts.build
+    post.body = 'foobar'
+    expect(post).to be_invalid    
+  end
+
+  it "is invalid without a body" do
+    post = @user.posts.build
+    post.title = 'hoge'
+    expect(post).to be_invalid    
+  end
+  it "is invalid without a user_id" do
+    post = Post.new(title: 'hoge', body: 'foobar')
+    expect(post).to be_invalid
+  end
 end
