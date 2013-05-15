61e3b27b7000da7ba395801efb9b66e3ccdaf5af
Ryo Hashimoto <ryohashimoto@gmail.com>
2013-05-15 13:09:16 +0900

    rewrite user model specs

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,26 +1,28 @@
 require 'spec_helper'
 
 describe User do
+  let(:user) do
+    build(:user)    
+  end
+
   it "is valid with a name, email and password" do
-    user = User.new(name: 'ryo', email: 'ryo@example.com', password: 'hogehoge')
     expect(user).to be_valid
   end
   it "is invalid without a name" do
-    user = User.new(email: 'ryo@example.com', password: 'hogehoge')
+    user.name = nil
     expect(user).to be_invalid
   end
   it "is invalid without an email" do
-    user = User.new(name: 'ryo', password: 'hogehoge')
+    user.email = nil
     expect(user).to be_invalid    
   end
   it "is invalid without a password" do
-    user = User.new(name: 'ryo', email: 'ryo@example.com')
+    user.password = nil
     expect(user).to be_invalid
   end
   it "is invalid with a duplicate email address" do
-    user1 = User.new(name: 'ryo', email: 'ryo@example.com', password: 'hogehoge')
-    expect(user1.save!).to be_true
-    user2 = User.new(name: 'hashimoto', email: 'ryo@example.com', password: 'hogehoge')
+    expect(user.save!).to be_true
+    user2 = User.new(name: 'hashimoto', email: user.email, password: 'hogehoge')
     expect(user2).to be_invalid
   end
 end
