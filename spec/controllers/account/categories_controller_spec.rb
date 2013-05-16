e1df27396ebdbc35f784a9583bbd23d16fbea740
Ryo Hashimoto <ryohashimoto@gmail.com>
2013-05-16 15:40:12 +0900

    add spec for CategoriesController

--- spec/controllers/account/categories_controller_spec.rb
+++ spec/controllers/account/categories_controller_spec.rb
@@ -1,5 +1,4 @@
 require 'spec_helper'
 
 describe Account::CategoriesController do
-
 end
--- spec/controllers/categories_controller_spec.rb
+++ spec/controllers/categories_controller_spec.rb
@@ -1,9 +1,48 @@
 require 'spec_helper'
 
 describe CategoriesController do
-
-  describe "GET 'show'" do
-    it "returns http success"
+  let(:user) { create(:user) }
+  let(:category1) do
+    category = user.categories.build
+    category.name = "ruby"
+    category.save!
+    category
+  end
+  let(:post1) do
+    post = user.posts.build
+    post.title = "Dolorum rerum minus est."
+    post.body = "Ratione sapiente deleniti nemo iusto voluptatem "
+    post.published_at = Time.now
+    categorization = post.categorizations.build
+    categorization.category_id = category1.id
+    categorization.save!
+    post.save!
+    post
   end
 
+  describe 'GET #show' do
+    context "when accessing using category's id" do
+      it "popurates an array of posts for a category" do
+        get :show, id: category1
+        expect(assigns(:posts)).to match_array [post1]
+      end
+      
+      it "renders the :show template" do
+        get :show, id: category1
+        expect(response).to render_template :show
+      end
+    end
+
+    context "when accessing using category's slug" do
+      it "popurates an array of posts for a category" do
+        get :show, id: category1.slug
+        expect(assigns(:posts)).to match_array [post1]
+      end
+      
+      it "renders the :show template" do
+        get :show, id: category1.slug
+        expect(response).to render_template :show
+      end
+    end
+  end
 end
