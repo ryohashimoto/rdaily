dc4ff48a1caa0dec43eca6d5e79f387622eb271b
Ryo Hashimoto <ryohashimoto@gmail.com>
2013-06-07 20:51:27 +0900

    add archives sidebar

--- app/controllers/archives_controller.rb
+++ app/controllers/archives_controller.rb
@@ -1,2 +1,11 @@
 class ArchivesController < ApplicationController
+  def index
+    @posts = resources.order("created_at desc").page(params[:page]).per(5)
+    @monthly_posts = @posts.group_by { |t| t.created_at.beginning_of_month }
+  end
+
+  private
+  def resources
+    Post.published
+  end
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,7 @@
 class PostsController < ApplicationController
   def index
     @posts = resources.order("created_at desc").page(params[:page]).per(5)
+    @monthly_posts = @posts.group_by { |t| t.created_at.beginning_of_month }
   end
 
   def show
