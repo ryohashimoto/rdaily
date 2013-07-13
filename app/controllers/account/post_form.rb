093bbeb4b4595e0aec5b99ee198ec1bafbb89d07
Ryo Hashimoto <ryohashimoto@gmail.com>
2013-07-13 10:56:57 +0900

    remove duplicated line

--- app/controllers/account/post_form.rb
+++ app/controllers/account/post_form.rb
@@ -17,10 +17,8 @@ module Account
     def set_category_ids(params)
       ids = params[:category_ids]
       if ids
-        if ids == "0"
-          @category_ids = []
-        else
-          @category_ids = []
+        @category_ids = []
+        if ids != "0"
           @category_ids = ids.split(',').map(&:to_i)
           if @category_ids[0] == 0
             @category_ids.shift
