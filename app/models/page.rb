cec411857f49fc2608315a010921f1079247b151
Ryo Hashimoto <ryohashimoto@gmail.com>
2013-07-16 20:37:02 +0900

    add slug to page model

--- app/models/page.rb
+++ app/models/page.rb
@@ -4,6 +4,7 @@ class Page < ActiveRecord::Base
   attr_accessible :title, :body, :published_at
   
   validates :title, :body, :user_id, presence: true
+  validates :slug, uniqueness: true, format: { with: /^[a-z][_a-z0-9]*/i }, length: { maximum: 16 }
   
   scope :published, where("published_at is not null")
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130716043311) do
+ActiveRecord::Schema.define(:version => 20130716112736) do
 
   create_table "categories", :force => true do |t|
     t.string   "name"
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(:version => 20130716043311) do
     t.datetime "published_at"
     t.datetime "created_at",   :null => false
     t.datetime "updated_at",   :null => false
+    t.string   "slug"
   end
 
   add_index "pages", ["id"], :name => "index_pages_on_id", :unique => true
