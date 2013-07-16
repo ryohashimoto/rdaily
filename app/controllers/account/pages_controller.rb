25db15ad5df82fa51c9c0a0f30e421ba296804e2
Ryo Hashimoto <ryohashimoto@gmail.com>
2013-07-16 13:43:00 +0900

    add skelton account/pages controller methods

--- app/controllers/account/pages_controller.rb
+++ app/controllers/account/pages_controller.rb
@@ -1,2 +1,32 @@
-class Account::PagesController < ApplicationController
+class Account::PagesController < Account::BaseController
+  layout 'account'
+  
+  def index
+  end
+  
+  def show
+  end
+  
+  def new    
+  end
+  
+  def edit
+  end
+  
+  def update
+  end
+  
+  def destroy
+  end
+  
+  def publish
+  end
+  
+  def unpublish
+  end
+  
+  private
+  def resources
+    current_user.pages
+  end
 end
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,2 +1,11 @@
 class PagesController < ApplicationController
+
+  def show
+    @post = resources.find(params[:id])
+  end
+  
+  private
+  def resources
+    Page.published
+  end
 end
