df540eda282476bf3eb12f6da05b846aa04f431c
Ryo Hashimoto <ryohashimoto@gmail.com>
2013-07-16 13:38:17 +0900

    set model content for page

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,3 +1,11 @@
 class Page < ActiveRecord::Base
-  attr_accessible :body, :published_at, :sequence, :title, :user_id
+  attr_accessible :title, :body, :published_at
+  
+  validates :title, :body, :user_id, presence: true
+  
+  scope :published, where("published_at is not null")
+
+  def published_policy
+    @published_policy ||= PublishedPolicy.new(self)
+  end
 end
