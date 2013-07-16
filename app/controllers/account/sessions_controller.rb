5a5a478bb86ffafd6882e4bab8197bc0182034a1
Ryo Hashimoto <ryohashimoto@gmail.com>
2013-07-16 10:52:37 +0900

    fix log out alert

--- app/controllers/account/sessions_controller.rb
+++ app/controllers/account/sessions_controller.rb
@@ -16,6 +16,6 @@ class Account::SessionsController < ApplicationController
 
   def destroy
     session[:user_id] = nil
-    redirect_to root_path, notice => "Logged out."
+    redirect_to root_path, :notice => "Logged out."
   end
 end
