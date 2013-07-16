40157e2093d3f7a0c9b46639b2efa5b5aef41a20
Ryo Hashimoto <ryohashimoto@gmail.com>
2013-07-16 20:43:43 +0900

    add presence: true to slug in page model

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,10 +1,10 @@
 class Page < ActiveRecord::Base
   belongs_to :user
   
-  attr_accessible :title, :body, :published_at
+  attr_accessible :title, :body, :slug, :published_at
   
   validates :title, :body, :user_id, presence: true
-  validates :slug, uniqueness: true, format: { with: /^[a-z][_a-z0-9]*/i }, length: { maximum: 16 }
+  validates :slug, presence: true, uniqueness: true, format: { with: /^[a-z][_a-z0-9]*/i }, length: { maximum: 16 }
   
   scope :published, where("published_at is not null")
 
