bbb238835ee37b3a5b775be20cc1b1e3af5623b5
Ryo Hashimoto <ryohashimoto@gmail.com>
2013-07-20 22:26:52 +0900

    add link for initial setup

--- db/migrate/20130719060032_add_type_to_posts.rb
+++ db/migrate/20130719060032_add_type_to_posts.rb
@@ -1,5 +1,10 @@
 class AddTypeToPosts < ActiveRecord::Migration
   def change
     add_column :posts, :type, :string
+    
+    Post.find_each do |post|
+      post.type = 'Post'
+      post.save!
+    end
   end
 end
