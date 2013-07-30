3e1e0441e52889a33a2b71f7b7c8fce8bbf11898
Ryo Hashimoto <ryohashimoto@gmail.com>
2013-07-30 21:42:22 +0900

    hstore extension statement in migration

--- db/migrate/20130726131254_create_products.rb
+++ db/migrate/20130726131254_create_products.rb
@@ -1,5 +1,13 @@
 class CreateProducts < ActiveRecord::Migration
   def change
+    reversible do |dir|
+      dir.up do
+        execute("create extension if not exists hstore")
+      end
+      dir.down do
+        execute("drop extension hstore")
+      end 
+    end
     create_table :products do |t|
       t.string :asin
       t.string :title
