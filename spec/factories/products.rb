fc385099614fa378cb8616393f9dedbbe83b362c
Ryo Hashimoto <ryohashimoto@gmail.com>
2013-12-07 21:48:01 +0900

    remove products from factories

--- spec/factories/products.rb
+++
@@ -1,9 +0,0 @@
-# Read about factories at https://github.com/thoughtbot/factory_girl
-
-FactoryGirl.define do
-  factory :product do
-    asin "MyString"
-    title "MyString"
-    data ""
-  end
-end
