29822c598e138f15789c063b781d8335c0e6d1bb
Ryo Hashimoto <ryohashimoto@gmail.com>
2015-06-13 23:35:45 +0900

    change to config.serve_static_files

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -11,7 +11,7 @@ Rdaily::Application.configure do
 
   config.assets.js_compressor = :uglifier
   # Disable Rails's static asset server (Apache or nginx will already do this)
-  config.serve_static_assets = false
+  config.serve_static_files = false
 
   # Compress JavaScripts and CSS
   config.assets.compress = true
