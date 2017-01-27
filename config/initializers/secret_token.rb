0052a27a8f6d2377c164a8530f5d47d33abf1c7d
Ryo Hashimoto <ryohashimoto@gmail.com>
2017-01-27 18:29:45 +0900

    use config/secrets.yml and remove secret_token.rb
    ENV['SECRET_KEY_BASE'] must be set in the production environment.

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Rdaily::Application.config.secret_token = 'ad85232743a15ee155e338075a614946e74c863393c16de324db7d8e2832a1f376271a8caf2baa8a9c04a7ceb59e81245142890226b01997cba0b5e351260ce4'
