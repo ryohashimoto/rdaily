89c49168072e60f363cd3060c6d2769b39529e92
Ryo Hashimoto <ryohashimoto@gmail.com>
2018-10-26 20:02:36 +0900

    update migration files

--- db/migrate/20130719060032_add_type_to_posts.rb
+++ db/migrate/20130719060032_add_type_to_posts.rb
@@ -1,4 +1,4 @@
-class AddTypeToPosts < ActiveRecord::Migration
+class AddTypeToPosts < ActiveRecord::Migration[4.2]
   def change
     add_column :posts, :type, :string
     
--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -16,64 +15,60 @@ ActiveRecord::Schema.define(version: 20130719060032) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "categories", force: :cascade do |t|
-    t.string   "name"
+  create_table "categories", id: :serial, force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.integer  "user_id"
-    t.string   "slug"
+    t.integer "user_id"
+    t.string "slug"
+    t.index ["slug"], name: "index_categories_on_slug"
   end
 
-  add_index "categories", ["slug"], name: "index_categories_on_slug", using: :btree
-
-  create_table "categorizations", force: :cascade do |t|
-    t.integer  "post_id"
-    t.integer  "category_id"
+  create_table "categorizations", id: :serial, force: :cascade do |t|
+    t.integer "post_id"
+    t.integer "category_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.index ["category_id"], name: "index_categorizations_on_category_id"
+    t.index ["post_id"], name: "index_categorizations_on_post_id"
   end
 
-  add_index "categorizations", ["category_id"], name: "index_categorizations_on_category_id", using: :btree
-  add_index "categorizations", ["post_id"], name: "index_categorizations_on_post_id", using: :btree
-
-  create_table "pages", force: :cascade do |t|
-    t.string   "title"
-    t.text     "body"
-    t.integer  "sequence"
-    t.integer  "user_id"
+  create_table "pages", id: :serial, force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.integer "sequence"
+    t.integer "user_id"
     t.datetime "published_at"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "slug"
+    t.string "slug"
+    t.index ["id"], name: "index_pages_on_id", unique: true
+    t.index ["published_at"], name: "index_pages_on_published_at"
   end
 
-  add_index "pages", ["id"], name: "index_pages_on_id", unique: true, using: :btree
-  add_index "pages", ["published_at"], name: "index_pages_on_published_at", using: :btree
-
-  create_table "photos", force: :cascade do |t|
-    t.string   "image",      null: false
-    t.integer  "user_id",    null: false
+  create_table "photos", id: :serial, force: :cascade do |t|
+    t.string "image", null: false
+    t.integer "user_id", null: false
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-  create_table "posts", force: :cascade do |t|
-    t.string   "title"
-    t.text     "body"
-    t.integer  "user_id"
+  create_table "posts", id: :serial, force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.integer "user_id"
     t.datetime "published_at"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "type"
+    t.string "type"
+    t.index ["id"], name: "index_posts_on_id", unique: true
+    t.index ["published_at"], name: "index_posts_on_published_at"
   end
 
-  add_index "posts", ["id"], name: "index_posts_on_id", unique: true, using: :btree
-  add_index "posts", ["published_at"], name: "index_posts_on_published_at", using: :btree
-
-  create_table "users", force: :cascade do |t|
-    t.string   "name"
-    t.string   "email"
-    t.string   "password_digest"
+  create_table "users", id: :serial, force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
